35d3e10d968b2e7ea5b257169eed57612e63c729
Vedangi <tvedangi@gmail.com>
2025-08-04 13:10:41 +0530

    type error issue solved

--- src/Contexts/PostContexts.ts
+++ src/Contexts/PostContexts.ts
@@ -2,8 +2,6 @@ import { createContext } from "react";
 import type { Post } from "../types/postTypes";
 
 export const PostContext = createContext<Post[]>([]);
-export const PostContext = createContext<Post[]>([]);
-export const PostContext = createContext<Post[]>([]);
-export const PostContext = createContext<Post[]>([]);
+
 
 
